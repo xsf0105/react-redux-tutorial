52e416a6b6ed46df6fad5cda53764b7883676d44
wb-xsf297196 <wb-xsf297196@alibaba-inc.com>
2017-12-18 20:37:40 +0800

    updata node package

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,32 +1,26 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import createLogger from 'redux-logger';
-import thunk from 'redux-thunk';
-import { hashHistory } from 'react-router';
-import { routerMiddleware } from 'react-router-redux';
-import rootReducer from '../app/reducers/';
+import { createStore, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+import { hashHistory } from "react-router";
+import { routerMiddleware } from "react-router-redux";
+import rootReducer from "../app/reducers/";
 
 const logger = createLogger();
 const router = routerMiddleware(hashHistory);
 
 const enhancer = compose(
-  applyMiddleware(
-    thunk,
-    logger,
-    router
-  ),
+  applyMiddleware(thunk, logger, router),
   window.devToolsExtension ? window.devToolsExtension() : f => f
 );
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer,
-    initialState, enhancer
-  );
+  const store = createStore(rootReducer, initialState, enhancer);
   window.store = store;
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../app/reducers/', () => {
-      const nextReducer = require('../app/reducers/').default; // eslint-disable-line global-require
+    module.hot.accept("../app/reducers/", () => {
+      const nextReducer = require("../app/reducers/").default; // eslint-disable-line global-require
       store.replaceReducer(nextReducer);
     });
   }
