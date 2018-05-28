9cb5ae1fdb375aea04e84f43d849c00091de48d0
Allan <alan@AllandeMacBook-Pro.local>
2018-05-28 15:35:19 +0800

    mock change

--- src/app/config/dip-api.js
+++ src/app/config/dip-api.js
@@ -1,12 +1,12 @@
 /**
  * 本地开发环境的mock数据
- * 当前使用了在线mock数据：http://www.mockhttp.cn
+ * 当前使用了在线mock数据：http://www.mockhttp.cn 
  */
 const server_host = "http://localhost:3001/mock/api/"; // script/server.js 中配置，代理到http://www.mockhttp.cn
 
 const API = {
   ENTRY: {
-    MESSAGE: server_host + "getSomeMessage"
+    MESSAGE: server_host + "songInfo"
   },
   HOME: {
     // MESSAGE:
