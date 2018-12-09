406274ad0057a2e265b05bcbfb27fb494db69287
Allan <allan91coder@gmail.com>
2018-12-09 22:06:16 +0800

    Fix remove useless file

--- pushdaily.sh
+++
@@ -1,9 +0,0 @@
-branch=`git rev-parse --abbrev-ref HEAD`;
-git pull origin master;
-git pull origin develop;
-git pull origin $branch;
-git push origin $branch;
-echo "部署daily到预发中.."
-# def p -d;
-npm run build;
-sh ./ispublish.sh;
